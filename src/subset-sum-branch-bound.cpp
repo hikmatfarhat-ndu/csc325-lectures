7d4048b71512d0fdf1505944fb62ddcc74e47dfc
Hikmat Farhat <hfarhat@ndu.edu.lb>
2020-04-21 14:52:23 +0300

    updated subsetsum branch and bound c++ code

--- src/subset-sum-branch-bound.cpp
+++ src/subset-sum-branch-bound.cpp
@@ -27,12 +27,8 @@ struct Node {
 };
 void backtrack(Node*& t, string path, string& solution) {
     if (solution.size() > 0)return;
-    if (t != nullptr) {
-        //std::cout << "Node sum= " << t->sum << " remainder= "
-          //  << t->remainder << " next= " << (t->next==s.end()?0:*t->next) << std::endl;
-        if (t->next != s.end()) {
+    if (t->next != s.end()) {
             if (t->sum + *t->next == target) {
-                cout << path + "," + to_string(*t->next) << " is the solution\n";
                 solution = path + "," + to_string(*t->next);
                 return;
             }
@@ -47,77 +43,86 @@ void backtrack(Node*& t, string path, string& solution) {
             backtrack(t->left, path + "," + to_string(*t->next), solution);
 
             backtrack(t->right, path, solution);
-        }
     }
-    else return;
 }
 
 void bb(Node* &t,string path,string& solution) {
     if (solution.size()>0)return;
-    if (t != nullptr) {
-        //std::cout << "Node sum= " << t->sum << " remainder= "
-          //  << t->remainder << " next= " << (t->next==s.end()?0:*t->next) << std::endl;
-        if (t->next != s.end()) {
-            if (t->sum + *t->next == target) {
-                cout << path+","+to_string(*t->next)<< " is the solution\n";
-                solution = path + "," + to_string(*t->next);
-                return;
-            }
-            if (t->sum + t->remainder <target)return;
-            t->left = new Node;
-            t->left->sum = t->sum + *t->next;
-            t->left->next = t->next + 1;
-            t->left->remainder = t->remainder - *t->next;
-            t->right = new Node;
-            t->right->sum = t->sum;
-            t->right->next = t->next + 1;
-            t->right->remainder = t->remainder - *t->next;
-            if (t->left->sum< target)
-                bb(t->left,path+","+to_string(*t->next),solution);
-            
-            bb(t->right,path,solution);
+    if (t->next != s.end()) {
+        if (t->sum + *t->next == target) {
+            solution = path + "," + to_string(*t->next);
+            return;
         }
+        if (t->sum + t->remainder < target)return;
+        t->left = new Node;
+        t->left->sum = t->sum + *t->next;
+        t->left->next = t->next + 1;
+        t->left->remainder = t->remainder - *t->next;
+        t->right = new Node;
+        t->right->sum = t->sum;
+        t->right->next = t->next + 1;
+        t->right->remainder = t->remainder - *t->next;
+        if (t->left->sum < target)
+            bb(t->left, path + "," + to_string(*t->next), solution);
+
+        bb(t->right, path, solution);
     }
-    else return;
 }
 int main()
-{
-    int set_size = 20;
-    for (int i = 0; i < set_size; i++)
-        s.push_back(id(e));
-    target = id(e);
-    for (auto x : s)
-        cout << x << ",";
-    /*s = { 4,12,18,22,100,200,300,4};
-    target = 17;*/
-    cout << "target =" << target << endl;
-    Node* root;
-    string solution = "";
-
-    {root  = new Node;
-    root->remainder = std::accumulate(s.begin(), s.end(), 0);
-    root->sum = 0;
-    root->next = s.begin();
-    bb(root, "", solution);
-    string val;
-    istringstream iss = istringstream(solution);
-    int total{ 0 };
-    while (getline(iss, val, ','))
-        total += atoi(val.c_str());
-
-    cout << "number= " << ::count << endl;
-    if (total!=0 && total != target)cout << "error\n";
-    }
-    
-    {
-        root = new Node;
+{//do 10 trials
+    for (int k = 0; k < 10; k++) {
+        s.clear();
         ::count = 0;
-        solution = "";
+        cout << " Trial " << k << endl;
+        cout << "--------------------------\n";
+        int set_size = 20;
+        for (int i = 0; i < set_size; i++)
+            s.push_back(id(e));
+        target = id(e);
+        for (auto x : s)
+            cout << x << ",";
+        cout << "target =" << target << endl;
+        Node* root;
+        string solution = "";
+
+        {root = new Node;
         root->remainder = std::accumulate(s.begin(), s.end(), 0);
         root->sum = 0;
         root->next = s.begin();
-        backtrack(root, "", solution);
-        cout << "number= " << ::count << endl;
+        bb(root, "", solution);
+
+        int total{ 0 };
+        if (solution.size() > 0) {
+
+            string val;
+            istringstream iss = istringstream(solution);
+            solution[0] = '[';
+            solution = solution + ']';
+            cout << "bb solution is-> " << solution << endl;
+            while (getline(iss, val, ','))
+                total += atoi(val.c_str());
+        }
+        cout << "nodes created bb->" << ::count << endl;
+        if (total != 0 && total != target)cout << "error\n";
+        }
+
+        {
+            root = new Node;
+            ::count = 0;
+            solution = "";
+            root->remainder = std::accumulate(s.begin(), s.end(), 0);
+            root->sum = 0;
+            root->next = s.begin();
+            backtrack(root, "", solution);
+            if (solution.size() > 0) {
+                solution[0] = '[';
+                solution = solution + ']';
+                cout << "backtracking solution is-> " << solution << endl;
+            }
+            cout << "nodes created backtracking->" << ::count << endl;
+        }
+        cout << "--------------------------\n";
+
     }
 
 }
